4090873cc23a6d9953080c2067d5b8a2cb8e99d5
davideusberti <72393528+UsboKirishima@users.noreply.github.com>
2023-03-29 22:12:13 +0200

    fix(converter): Formatted old code with a new lightweight and fresh code
    This commit just for fix the old shit made on this project by Lunar, I'm the freshest man on this planet, so i freshed this code WWWWW

--- src/convert.h
+++ src/convert.h
@@ -1,28 +1,23 @@
+#include <alloca.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void convert_to_morse(char string[100]){
-        printf("\n");
-	for(int i = 0; i < strlen(string); i++){
-		char MORSE_ALPHABET[42][7] = {
+#define TOLOWER(ch) (((ch) >= 'A' && (ch) <= 'Z') ? ((ch) - 'A' + 'a') : (ch))
+
+static char MORSE_ALPHABET[42][30] = {
     ".-",    "-...",    "-.-.",   "-..",    ".",      "..-.",   "--.",
     "....",  "..",      ".---",   "-.-",    ".-..",   "--",     "-.",
     "---",   ".--.",    "--.-",   ".-.",    "...",    "-",      "..--",
     "...-",  ".--",     "-..-",   "-.--",   "--..",   ".----",  "..---",
     "...--", "....-",   ".....",  "-....",  "--...",  "---..",  "----.",
     "-----", ".......", "--..--", ".-.-.-", "---...", "..--..", "-.-.--"};
-   		int temp = 0;
-    		char FRASE[100];
-    		system("cls||clear");
-    		printf("Inserisci la frase da convertire: ");
-    		gets(FRASE);
-    		for(int i = 0; i < strlen(FRASE); i++){
-        		if(FRASE[i]  >= 'A' && FRASE[i] <= 'Z'){
-            		FRASE[i] += 32;
-        		}
-        		temp = FRASE[i] - 97;
-        		printf("%s ", MORSE_ALPHABET[temp]);
-    		}
-	}
+
+void convert_to_morse(char string[40]) {
+  int temp = 0;
+  for (int i = 0; i < strlen(string); i++) {
+  char formatted_string = TOLOWER(string[i]);
+    temp = formatted_string - 97;
+    printf("%s ", MORSE_ALPHABET[temp]);
+  }
 }
